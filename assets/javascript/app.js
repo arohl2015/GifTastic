7c4319e37636582ed235f00f2f1e6a232d07e6b8
A Rohl <april.rohl2015@gmail.com>
2020-03-07 19:53:44 -0700

    created array in JS file

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Start of Game
+
+//Create topic array to hold my initial button variables
+
+var topicDisney = [
+    "Aladdin", "Toy Story", "The Little Mermaid", "The Lion King", "Finding Nemo",
+    "Mickey Mouse", "Olaf", "Maleficent", "Cinderella", "Mary Poppins"
+];
+//Global variables for topic
+var topicSelected = "";
+var topicNew;
+
+//Create the buttons within my webpage
+
+function renderButton () {
+    $("#topicDisney-view").empty();
+    for (var i=0; i < topicDisney.length; i++;
+        
+    }
+}
